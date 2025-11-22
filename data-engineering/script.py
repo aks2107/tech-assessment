2346432e749a5214c77c603edb6a3f8aea1c0f28
aswar <aks2107@rit.edu>
2025-11-22 16:35:57 -0500

    Added functionality for reading and processing memberPaidInfo.csv

--- data-engineering/script.py
+++ data-engineering/script.py
@@ -29,17 +29,38 @@ def check_directories():
 def read_member_info():
     """
     This function reads the information from the memberInfo.csv file into a dictionary for quick lookups.
-    Returns: A dictionary format with the key-value pairs.
+    Returns: A dictionary with member ID as the key and the member name as the value.
     """
 
-    members = {} # Initialize empty dictionary
+    members = {} # Initialize empty members dictionary
     with open(MEMBERINFO_FILE, 'r') as file:
         reader = csv.DictReader(file) # First line in file gets treated as keys which are the columns and everything following as values for the keys
         for row in reader: 
-            member_id = row['id'].strip # Retrieve the 'id' value for that specific row
+            member_id = row['id'].strip() # Retrieve the 'id' value for that specific row
             member_name = row['name'].strip() # Retrieve the 'name' value for that specific row
             members[member_id] = member_name # Populate dictionary with key as member id and value as member name
     return members
 
+def read_member_paid_info():
+    """
+    This function reads the information from the memberPaidInfo.csv file into a dictionary for quick lookups.
+    Returns: A dictionary with transaction data.
+    """
 
-
+    transactions = {} # Initialize empty transactions dictionary
+    with open(MEMBERPAIDINFO_FILE, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            t_id = row['id'].strip()
+            t_name = row['name'].strip()
+            # Try and except block used to handle price data that may not be numerical
+            try:
+                t_price = float(row['price']) # Convert price to floating number
+            except ValueError: # If the price value cannot be converted to floating number
+                continue # Skip that data and move to next
+            transactions.append({ # Append to transactions dictionary
+                'id': t_id,
+                'name': t_name,
+                'price': t_price
+            })
+    return transactions
