a4f5c6c08a0612426f1790696d40931aca8f47a4
aswar <aks2107@rit.edu>
2025-11-22 15:43:19 -0500

    Added functionality for reading memberInfo

--- data-engineering/script.py
+++ data-engineering/script.py
@@ -13,6 +13,7 @@ def check_directories():
     If an output directory is missing it gets created.
     If the input files and directory is not present then the program terminates alerting the user that something is missing.
     """
+
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
     if not os.path.exists(DATA_DIR):
@@ -24,6 +25,21 @@ def check_directories():
     if not os.path.exists(MEMBERPAIDINFO_FILE):
          print("memberPaidInfo.csv is missing and the script will terminate.")
          exit(1)
+
+def read_member_info():
+    """
+    This function reads the information from the memberInfo.csv file into a dictionary for quick lookups.
+    Returns: A dictionary format with the key-value pairs.
+    """
+
+    members = {} # Initialize empty dictionary
+    with open(MEMBERINFO_FILE, 'r') as file:
+        reader = csv.DictReader(file) # First line in file gets treated as keys which are the columns and everything following as values for the keys
+        for row in reader: 
+            member_id = row['id'].strip # Retrieve the 'id' value for that specific row
+            member_name = row['name'].strip() # Retrieve the 'name' value for that specific row
+            members[member_id] = member_name # Populate dictionary with key as member id and value as member name
+    return members
 
 
 
