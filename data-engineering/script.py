251b218631202a042e7f049b5ab3a1cab492158d
aswar <aks2107@rit.edu>
2025-11-24 18:49:01 -0500

    Completed implementation and fixed logical errors

--- data-engineering/script.py
+++ data-engineering/script.py
@@ -11,7 +11,7 @@ def check_directories():
     """
     This function checks to make sure the required directories are present. 
     If an output directory is missing it gets created.
-    If the input files and directory is not present then the program terminates alerting the user that something is missing.
+    If the input files or data directory is not present then the program terminates alerting the user of the missing item.
     """
 
     if not os.path.exists(OUTPUT_DIR):
@@ -28,24 +28,24 @@ def check_directories():
 
 def read_member_info():
     """
-    This function reads the information from the memberInfo.csv file into a dictionary for quick lookups.
+    This function reads the information from memberInfo.csv into a dictionary for quick lookups.
     Returns: A dictionary with member ID as the key and the member full name as the value.
     """
 
     members = {} # Initialize empty members dictionary
     with open(MEMBERINFO_FILE, 'r') as file:
-        reader = csv.DictReader(file) # First line in file gets treated as keys which are the columns and everything following as values for the keys
+        reader = csv.DictReader(file) # First line in file gets treated as keys which are the columns and everything following gets treated as values for the keys
         for row in reader: 
             member_id = row['memberId'].strip() # Retrieve the 'memberID' value for that specific row
             first_name = row['firstName'].strip() # Retrieve the 'firstName' value for that specific row
             last_name = row['lastName'].strip() # Retrieve the 'lastName' value for that specific row
-            full_name = f"{first_name} {last_name}".strip() # Combine first and last name into one string
+            full_name = f"{first_name} {last_name}".strip() # Combine first and last name into one string and remove white space
             members[member_id] = full_name # Populate dictionary with key as member id and value as full name
     return members
 
 def read_member_paid_info():
     """
-    This function reads the information from the memberPaidInfo.csv file into a dictionary for quick lookups.
+    This function reads the information from memberPaidInfo.csv file into a dictionary for quick lookups.
     Returns: A list of dictionaries with transaction data.
     """
 
@@ -53,13 +53,13 @@ def read_member_paid_info():
     with open(MEMBERPAIDINFO_FILE, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            t_id = row['memberId'].strip()
-            t_name = row['fullName'].strip()
+            t_id = row['memberId'].strip() # Get 'memberID' value for that specific row
+            t_name = row['fullName'].strip() # Get 'fullName' value for that specific row
             # Try and except block used to handle price data that may not be numerical
             try:
-                t_price = float(row['paidAmount']) # Convert price to floating number
-            except ValueError: # If the price value cannot be converted to floating number
-                continue # Skip that data and move to next
+                t_price = int(row['paidAmount']) # Convert price to int
+            except ValueError: # If the price value cannot be converted to int
+                continue # Skip row and move to next
             transactions.append({ # Append to transactions list
                 'ID': t_id,
                 'fullName': t_name,
@@ -72,11 +72,11 @@ def main():
     This function is the main function that does the following:
     1. Checks if the required directories and files exist
     2. Gets list of transactions
-    3. Goes through list of transactions and finds if the member name and member ID exist
+    3. Goes through list of transactions and checks if a valid member exists for that transaction and gets rid of conflicting names
     4. Gets total paid amount
     5. Gets highest paying member
-    6. Creates output file with the clean data
-    7. Prints out some insights about the clean data
+    6. Creates output file with the clean entries
+    7. Prints out some insights about the clean entires
     """
     
     check_directories() # Check to make sure directories and files exist before moving on
@@ -84,34 +84,34 @@ def main():
     transactions = read_member_paid_info() # Get all the transactions
     total_paid = 0.0
     clean_entries = []
-    highest_payer = {'fullName': None, 'paidAmount': 0.0, 'memberID': None}
+    highest_payer = {'fullName': None, 'paidAmount': 0, 'memberID': None}
     for row in transactions:
         payment_id = row['ID']
         payment_name = row['fullName']
         payment_amount = row['paidAmount']
-        if payment_id not in members: # Check if ID exists in member list
+        if payment_id not in members: # Check if ID found in transactions exists in member information list
             continue # Skip ID if not associated with member
-        if (members[payment_id].upper() != payment_name.upper()) and (payment_name != '') and (members[payment_id] != ''): # Check if names match without capitalization issues
-            continue # Skip name if not associated with member
+        if (members[payment_id].upper() != payment_name.upper()) and (payment_name != '') and (members[payment_id] != ''): # Check for conflicting names
+            continue # Skip name if it is conflicting with names in member list
+        if(payment_name == ''): # Check if name in transaction list is empty
+            payment_name = members[payment_id] # Fill in the name of member for the transaction
         total_paid += payment_amount # Add to total paid count
-        if payment_amount > highest_payer['paidAmount']:
+        if payment_amount > highest_payer['paidAmount']: # Check if transaction amount if greater than the current highest paid amount
             highest_payer = {'fullName': payment_name, 'paidAmount': payment_amount, 'memberID': payment_id} # Update highest payer data
         clean_entries.append({ # Add valid member transaction information to list of clean entries
-            'memberId': payment_id,
+            'memberID': payment_id,
             'fullName': payment_name,
             'paidAmount': payment_amount
         })
-    for entry in clean_entries:
-        print(entry)
     with open(OUTPUT_FILE, 'w', newline='') as file:
-        columns = ['memberID', 'fullName', 'paidAmount']
+        columns = ['memberID', 'fullName', 'paidAmount'] # Columns for output file
         writer = csv.DictWriter(file, columns)
         writer.writeheader() # Make the first line in the file the column names
         writer.writerows(clean_entries) # The rest of the rows in the file are the clean entries
     # Print out some insights about the clean entires
-    print(f"Total clean rows: {len(clean_entries)}")
+    print(f"Total Clean Entries: {len(clean_entries)}")
     print(f"Total Paid Amount: ${total_paid}")
-    print(f"Highest Payer: {highest_payer['fullName']}, ID: {highest_payer['memberID']}, Amount: {highest_payer['paidAmount']}")
+    print(f"Highest Payer Name: {highest_payer['fullName']}, ID: {highest_payer['memberID']}, Paid Amount: ${highest_payer['paidAmount']}")
 
 if __name__ == "__main__":
     main()
